2025b7fb1735ca9ac9d26394c5ecbad86b2f116c
georgeboiko <96927243+georgeboiko@users.noreply.github.com>
2023-01-24 23:43:18 +0300

    Create main.cpp
    Шапка программ

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <cmath>
+#include <numeric>
+#include <set>
+#include <map>
+#include <stack>
+
+using namespace std;
+
+#define el '\n'
+#define fastIO ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define all(x) (x).begin(), (x).end()
+
+typedef long long ll;
+typedef long double ld;
+
+int main() {
+	fastIO;
+	
+}
