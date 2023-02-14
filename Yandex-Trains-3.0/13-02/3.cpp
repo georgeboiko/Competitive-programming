545590234aaba429ded360515367750b18d45cf6
georgeboiko <96927243+georgeboiko@users.noreply.github.com>
2023-02-15 00:48:11 +0300

    Create 3.cpp

---
+++ Yandex-Trains-3.0/13-02/3.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <cmath>
+#include <numeric>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+#define el '\n'
+#define fastIO ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define all(x) (x).begin(), (x).end()
+
+typedef long long ll;
+typedef long double ld;
+
+const ll INF = 1e14;
+
+int main() {
+	fastIO;
+	ll n;
+	cin >> n;
+	vector<ll>a(n);
+	for (ll& i : a) cin >> i;
+	sort(all(a));
+	ll m;
+	cin >> m;
+	while (m--) {
+		ll t;
+		cin >> t;
+		cout << lower_bound(all(a), t) - a.begin() << el;
+	}
+}
