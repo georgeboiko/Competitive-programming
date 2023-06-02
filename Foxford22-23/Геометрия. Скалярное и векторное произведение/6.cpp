b9e5ca3ee519ae5bfa73d04ebd86da330ea13f1c
georgeboiko <96927243+georgeboiko@users.noreply.github.com>
2023-06-02 12:29:17 +0300

    Create 6.cpp

---
+++ Foxford22-23/Геометрия. Скалярное и векторное произведение/6.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <cmath>
+#include <numeric>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+#define el '\n'
+#define fastIO ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define all(x) (x).begin(), (x).end()
+
+typedef long long ll;
+typedef long double ld;
+
+const ll INF = 1e14;
+
+struct Point {
+	ld x, y;
+	Point() {}
+	Point(ld x, ld y) : x(x), y(y) {}
+};
+
+inline Point operator - (const Point& a, const Point& b) {
+	return Point(a.x - b.x, a.y - b.y);
+}
+
+inline ld len(const Point& a) {
+	return hypot(a.x, a.y);
+}
+
+inline ld dP(const Point& a, const Point& b) {
+	return a.x * b.x + a.y * b.y;
+}
+
+inline ld cP(const Point& a, const Point& b) {
+	return a.x * b.y - a.y * b.x;
+}
+
+istream& operator >> (istream& in, Point& p) {
+	in >> p.x >> p.y;
+	return in;
+}
+
+inline ld angle(Point& a, Point& b) {
+	return atan2(cP(a, b), dP(a, b));
+}
+
+inline bool operator == (const Point& a, const Point& b) {
+	return (a.x == b.x && a.y == b.y);
+}
+
+inline bool pointOnRay(const Point& p, const Point& a, const Point& b) {
+	return cP(p - a, b - a) == 0 && dP(p - a, b - a) >= 0;
+}
+
+inline bool pointOnSeg(const Point& p, const Point& a, const Point& b) {
+	return pointOnRay(p, a, b) && pointOnRay(p, b, a);
+}
+
+int main() {
+	Point p, a, b;
+	cin >> p >> a >> b;
+	if (dP(p - a, b - a) >= 0 && dP(p - b, a - b) >= 0) {
+		cout << abs(cP(b - a, p - a)) / len(a - b) << el;
+	}
+	else {
+		cout << min(len(p - a), len(p - b)) << el;
+	}
+}
